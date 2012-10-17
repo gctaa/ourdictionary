bbd715c3b5764ff272e087eac81365842e7cce7f
Matthew Gallagher <mattva01@gmail.com>
2012-10-17 14:36:26 -0400

    Added example path to wsgi.pyx

--- wsgi.py
+++ wsgi.py
@@ -14,8 +14,11 @@ framework.
 
 """
 import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+import sys
+path = '/opt/ourdictionary/'
+if path not in sys.path:
+    sys.path.append(path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ourdictionary.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
